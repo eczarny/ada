146f3d8e12133524d41c6ae0faae3ad41f67876c
Eric Czarny <eczarny@gmail.com>
2011-08-02 21:27:21 -0400

    Added a simple trait to permit easier logging.

---
+++ src/main/scala/com/divisiblebyzero/ada/Main.scala
@@ -0,0 +1,32 @@
+package com.divisiblebyzero.ada
+
+//
+// Main.scala
+// Ada Chess
+//
+// Created by Eric Czarny on August 1, 2011.
+// Copyright 2011 Divisible by Zero. All rights reserved.
+//
+
+import javax.swing.UIManager
+import javax.swing.UnsupportedLookAndFeelException
+import javax.swing.plaf.metal.MetalLookAndFeel
+
+import org.apache.log4j.Logger
+
+import com.divisiblebyzero.ada.common.Ada
+import com.divisiblebyzero.ada.common.Logging
+
+object Main extends Logging {
+  def launch(): Unit = new Ada()
+
+  def main(args: Array[String]): Unit = {
+    try {
+      UIManager.setLookAndFeel(new MetalLookAndFeel())
+    } catch {
+      case e: UnsupportedLookAndFeelException => error("The requested look and feel is not supported.")
+    }
+
+    Main.launch()
+  }
+}
---
+++ src/main/scala/com/divisiblebyzero/ada/common/Logging.scala
@@ -0,0 +1,34 @@
+package com.divisiblebyzero.ada.common
+
+//
+// Loggable.scala
+// Ada Chess
+//
+// Created by Eric Czarny on August 2, 2011.
+// Copyright 2011 Divisible by Zero. All rights reserved.
+//
+
+import org.apache.log4j.Logger
+
+trait Logging {
+  val loggerName = this.getClass.getName
+  lazy val logger = Logger.getLogger(loggerName)
+
+  def trace(message: String) = logger.trace(message)
+  def trace(message: String, e: Throwable) = logger.trace(message, e)
+
+  def debug(message: String) = logger.debug(message)
+  def debug(message: String, e: Throwable) = logger.debug(message, e)
+  
+  def info(message: String) = logger.info(message)
+  def info(message: String, e: Throwable) = logger.info(message, e)
+  
+  def warn(message: String) = logger.warn(message)
+  def warn(message: String, e: Throwable) = logger.warn(message, e)
+  
+  def error(message: String) = logger.error(message)
+  def error(message: String, e: Throwable) = logger.error(message, e)
+  
+  def fatal(message: String) = logger.fatal(message)
+  def fatal(message: String, e: Throwable) = logger.fatal(message, e)
+}
